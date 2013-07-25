8aff8053e337d3f3a0a4b8ac7daf4851bbb39cdb
Alex Kalderimis <alex.kalderimis@gmail.com>
2013-07-25 17:15:50 +0100

    Make cdn configurable

--- facetted-query-builder/config.js
+++ facetted-query-builder/config.js
@@ -1,4 +1,4 @@
-var config;
+var config, CDN = "http://cdn.intermine.org";
 
 module.exports = config = {
   "author": "Alex <alex@intermine.org>",
@@ -9,29 +9,29 @@ module.exports = config = {
   "dependencies": {
     "css": {
       "imtables-css": {
-        "path": "http://cdn.intermine.org/js/intermine/im-tables/1.3.0/imtables.css"
+        "path": CDN + "/js/intermine/im-tables/1.3.0/imtables.css"
       }
     },
     "js": {
       "JSON": {
-        "path": "http://cdn.intermine.org/js/json3/3.2.2/json3.min.js"
+        "path": CDN + "/js/json3/3.2.2/json3.min.js"
       },
       "jQuery": {
-        "path": "http://cdn.intermine.org/js/jquery/1.9.1/jquery-1.9.1.min.js"
+        "path": CDN + "/js/jquery/1.9.1/jquery-1.9.1.min.js"
       },
       "_": {
-        "path": "http://cdn.intermine.org/js/underscore.js/1.3.3/underscore-min.js"
+        "path": CDN + "/js/underscore.js/1.3.3/underscore-min.js"
       },
       "Backbone": {
-        "path": "http://cdn.intermine.org/js/backbone.js/1.0.0/backbone.js",
+        "path": CDN + "/js/backbone.js/1.0.0/backbone.js",
         "depends": ["jQuery", "_"]
       },
       "imjs": {
-        "path": "http://cdn.intermine.org/js/intermine/imjs/2.6.2/im.js",
+        "path": CDN + "/js/intermine/imjs/2.6.2/im.js",
         "depends": ["Backbone"]
       },
       "imtables": {
-        "path": "http://cdn.intermine.org/js/intermine/im-tables/1.3.0/imtables-mini-bundle.js",
+        "path": CDN + "/js/intermine/im-tables/1.3.0/imtables-mini-bundle.js",
         "depends": ["imjs"]
       }
     }
